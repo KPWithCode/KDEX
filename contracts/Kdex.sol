70f711c4e5a5eff4ea8bd68ab94448d6739db2c7
Kyle Phillips <khillips375@yahoo.com>
2020-08-02 19:44:10 -0400

    Functions for deposit and withdrawing of tokens

--- contracts/Kdex.sol
+++ contracts/Kdex.sol
@@ -1,5 +1,6 @@
 pragma solidity 0.6.3;
 
+import 'https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/IERC20.sol';
 contract KDex {
     struct Token {
         bytes32 ticker;
@@ -8,6 +9,7 @@ contract KDex {
     mapping(bytes32 => Token) public tokens;
     // list
     bytes32[] public tokenList;
+    mapping(address => mapping(bytes32 => uint)) public traderBalances
     address public admin;
 
     constructor() public {
@@ -20,6 +22,26 @@ contract KDex {
     external {
         tokens[ticker] = Token(ticker, tokenAddress);
         tokenList.push(ticker);
+    }
+
+    function deposit(uint amount, bytes32 ticker) tokenExist() external {
+        IERC20(tokens[ticker].tokenAddress).transferFrom(
+            msg.sender, 
+            address(this), 
+            amount);
+        transferBalances[msg.sender][ticker] += amount;
+    }
+    function withdraw(uint amount, bytes32 ticker) tokenExist() external {
+        require(transferBalances[msg.sender][ticker] >= amount, 'balance too low');
+        tranderBalances[msg.sender][ticker] -= amount;
+        IERC20(tokens[ticker].tokenAddress).transfer(msg.sender, amount);
+    }
+    
+    modifier tokenExist(bytes32 ticker) {
+        require(tokens[ticker].tokenAddress != address(0),
+        'this token does not exist'
+        );
+        _;
     }
     modifier onlyAdmin() {
         require(msg.sender == admin, 'only admin');
