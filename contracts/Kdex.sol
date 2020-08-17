158dd84287400aeb63aaf97e69ee4e7d55f138bb
Kyle Phillips <khillips375@yahoo.com>
2020-08-16 21:52:09 -0400

    Created function for limit orders

--- contracts/Kdex.sol
+++ contracts/Kdex.sol
@@ -2,15 +2,36 @@ pragma solidity 0.6.3;
 
 import 'https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/IERC20.sol';
 contract Kdex {
+
+     // two types of limit orders
+    enum Side {
+        BUY,
+        SELL
+    }
+
     struct Token {
         bytes32 ticker;
         address tokenAddress;
     }
+
+    struct Order {
+        uint id;
+        Side side;
+        bytes32 ticker;
+        uint amount;
+        uint filled;
+        uint price;
+        uint date;
+    }
+
     mapping(bytes32 => Token) public tokens;
     // list
     bytes32[] public tokenList;
-    mapping(address => mapping(bytes32 => uint)) public traderBalances
+    mapping(address => mapping(bytes32 => uint)) public traderBalances;
+    mapping(bytes32 => mapping(uint => Order[])) public orderBook;
     address public admin;
+    uint public nextOrderID;
+    bytes32 constant DAI = bytes32('DAI');
 
     constructor() public {
         admin = msg.sender;
@@ -35,6 +56,40 @@ contract Kdex {
         require(transferBalances[msg.sender][ticker] >= amount, 'balance too low');
         tranderBalances[msg.sender][ticker] -= amount;
         IERC20(tokens[ticker].tokenAddress).transfer(msg.sender, amount);
+    }
+    function createLimitOrder(bytes32 ticker, uint amount, uint price, Side side) tokenExist(ticker) external {
+        require(ticker != DAI, 'cannot trade DAI');
+        // trade has enough token in balance
+        if(side == Side.SELL) {
+            require(transferBalances[msg.sender][ticker] >= amount, 'token balance too low');
+        } else {
+            require(traderBalances[msg.sender][DAI] >= amount * price, 
+            'dai balance too low'
+            );
+        }
+        Order[] storage orders = orderBook[ticker][uint(side)];
+        orders.push(Order(
+        nextOrderId,
+        side,
+        ticker,
+        amount,
+        0,
+        price,
+        now,
+        ))
+        uint i = orders.length -  1;
+        while (i > 0) {
+            if(side == SIDE.BUY && orders[i - 1].price > orders[i].price) {
+                break;
+            }
+             if(side == SIDE.SELL && orders[i - 1].price < orders[i].price) {
+                break;
+            }
+            Order memory order = orders[i - 1];
+            orders[i - 1] = orders[i];
+            i--;
+        }
+        nextOrderId++;
     }
     
     modifier tokenExist(bytes32 ticker) {
