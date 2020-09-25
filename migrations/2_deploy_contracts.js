70aa16ad1755947b62d91a9763b960e5ef506a5a
Kyle Phillips <khillips375@yahoo.com>
2020-09-25 00:31:53 -0400

    Migration for deploying contracts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -6,3 +6,18 @@ const Kdex = artifacts.require('Kdex.sol');
 
 const [DAI, BAT, REP, ZRX] = ['DAI', 'BAT', 'REP', 'ZRX']
 .map(ticker => web3.utils.fromAscii(ticker));
+
+module.exports = async function(deployer) {
+    await Promise.all(
+        [Dai, Bat, Rep, Zrx, Kdex].map(contract => deployer.deploy(contract))
+    );
+    const [dai, bat, rep, zrx, dex] = await Promise.all(
+        [Dai, Bat, Rep, Zrx, Kdex].map(contract => contract.deployed())
+    );
+    await Promise.all([
+        dex.addToken(DAI, dai.address),
+        dex.addToken(BAT, bat.address),
+        dex.addToken(REP, rep.address),
+        dex.addToken(ZRX, zrx.address)
+    ]);
+}
