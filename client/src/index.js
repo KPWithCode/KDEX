ba32500c610f2fdfe7da195bee8d666d690a1097
Kyle Phillips <khillips375@yahoo.com>
2020-10-02 13:24:58 -0400

    Index renders loading screen not app.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Loading from './Loading';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Loading />
   </React.StrictMode>,
   document.getElementById('root')
 );
